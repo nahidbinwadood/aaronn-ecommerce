5c13769ed10346f745639ff26ad91d7ea37533a9
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-22 12:54:24 +0600

    navbar done

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,16 +1,43 @@
 import {
-    CartSvg,
+  CartSvg,
   DownArrowSvg,
+  HamburgerSvg,
   PersonSvg,
   SearchSvg,
 } from '@/Components/Svg Container/SvgContainer';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const navLinks = [
+    {
+      title: 'Dollar Express',
+      path: '/dollar-express',
+    },
+    {
+      title: 'Best sellers',
+      path: '/best-sellers',
+    },
+    {
+      title: 'Ships from us',
+      path: '/ships-from-us',
+    },
+    {
+      title: 'Home Improvement',
+      path: '/home-improvement',
+    },
+    {
+      title: 'Top Brands',
+      path: '/top-brands',
+    },
+    {
+      title: 'More',
+      path: '/more',
+    },
+  ];
   return (
-    <div className='font-sans'>
+    <div className="font-sans">
       {/* primary navbar */}
-      <div className="bg-primaryColor text-white h-[70px] flex items-center ">
+      <div className="bg-primaryColor text-white h-[70px] flex items-center">
         <div className="container mx-auto w-full flex items-center justify-between">
           <div>
             <Link to="/">
@@ -35,7 +62,7 @@ const Navbar = () => {
             </form>
           </div>
 
-          <div className='flex items-center gap-12'>
+          <div className="flex items-center gap-12">
             <div className="cursor-pointer">
               <div className="flex items-center gap-2">
                 <div className="size-6 rounded-full overflow-hidden">
@@ -56,27 +83,52 @@ const Navbar = () => {
                 </div>
               </div>
             </div>
-            <div className='flex items-center gap-2 cursor-pointer'>
+            <div className="flex items-center gap-2 cursor-pointer">
               <div>
                 <PersonSvg />
               </div>
               <div>
                 <p className="text-xs">Hello</p>
-                <div className='flex items-center gap-2'>
-                  <p className='text-sm'>Sign In Register</p>
+                <div className="flex items-center gap-2">
+                  <p className="text-sm">Sign In Register</p>
                   <DownArrowSvg />
                 </div>
               </div>
             </div>
-            <div className='flex items-center gap-2 cursor-pointer'>
-                <div>
-                    <CartSvg/>
-                </div>
-                <div>
-                    <p className='text-xs'>0</p>
-                    <p className='text-sm'>Cart</p>
-                </div>
+            <div className="flex items-center gap-2 cursor-pointer">
+              <div>
+                <CartSvg />
+              </div>
+              <div>
+                <p className="text-xs">0</p>
+                <p className="text-sm">Cart</p>
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-secondaryColor h-[80px] flex items-center">
+        <div className="container mx-auto flex items-center gap-40">
+          <div className="bg-white px-6 py-3 rounded-full flex items-center gap-10 w-fit cursor-pointer">
+            <div className="flex items-center gap-3">
+              <HamburgerSvg />
+              <p className="text-sm font-semibold">All Categories</p>
+            </div>
+            <div>
+              <DownArrowSvg dark={true} />
+            </div>
+          </div>
+          <div className="flex items-center gap-8">
+            {navLinks?.map((link) => (
+              <NavLink
+                key={link?.title}
+                to={link?.path}
+                className="flex items-center gap-1"
+              >
+                <p>{link?.title}</p>
+                {link?.path === '/more' ? <DownArrowSvg dark={true} /> : ''}
+              </NavLink>
+            ))}
           </div>
         </div>
       </div>
