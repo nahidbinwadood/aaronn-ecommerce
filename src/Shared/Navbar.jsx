d2df5b2c83dfd85cef25813e38e51ad300a7d9de
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-23 11:04:29 +0600

    updated category hover

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -5,35 +5,94 @@ import {
   PersonSvg,
   SearchSvg,
 } from '@/Components/Svg Container/SvgContainer';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
+const navLinks = [
+  {
+    title: 'Dollar Express',
+    path: '/dollar-express',
+  },
+  {
+    title: 'Best sellers',
+    path: '/best-sellers',
+  },
+  {
+    title: 'Ships from us',
+    path: '/ships-from-us',
+  },
+  {
+    title: 'Home Improvement',
+    path: '/home-improvement',
+  },
+  {
+    title: 'Top Brands',
+    path: '/top-brands',
+  },
+  {
+    title: 'More',
+    path: '/more',
+  },
+];
+const categories = [
+  {
+    title: 'Bag',
+    path: '/categories/bag',
+  },
+  {
+    title: 'Furniture',
+    path: '/categories/furniture',
+  },
+  {
+    title: 'Beauty & Health',
+    path: '/categories/beauty-&-health',
+  },
+  {
+    title: 'Toys & Games',
+    path: '/categories/toys-&-games',
+  },
+  {
+    title: 'Electronics',
+    path: '/categories/electronics',
+  },
+  {
+    title: 'Luggage & Bags',
+    path: '/categories/luggage-&-bags',
+  },
+  {
+    title: 'Shoes',
+    path: '/categories/shoes',
+  },
+  {
+    title: 'Home & Garden',
+    path: '/categories/home-&-garden',
+  },
+  {
+    title: 'Men',
+    path: '/categories/men',
+  },
+  {
+    title: 'Women',
+    path: '/categories/women',
+  },
+  {
+    title: 'Sport',
+    path: '/categories/sport',
+  },
+  {
+    title: 'Phones',
+    path: '/categories/phones',
+  },
+  {
+    title: 'Laptop',
+    path: '/categories/laptop',
+  },
+];
+
+const lastCategory = categories[categories.length - 1];
+console.log(lastCategory);
 const Navbar = () => {
-  const navLinks = [
-    {
-      title: 'Dollar Express',
-      path: '/dollar-express',
-    },
-    {
-      title: 'Best sellers',
-      path: '/best-sellers',
-    },
-    {
-      title: 'Ships from us',
-      path: '/ships-from-us',
-    },
-    {
-      title: 'Home Improvement',
-      path: '/home-improvement',
-    },
-    {
-      title: 'Top Brands',
-      path: '/top-brands',
-    },
-    {
-      title: 'More',
-      path: '/more',
-    },
-  ];
+  const [showCategory, setShowCategory] = useState(false);
   return (
     <div className="font-sans">
       {/* primary navbar */}
@@ -107,15 +166,52 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
+      {/* secondary navbar */}
       <div className="bg-secondaryColor h-[80px] flex items-center">
         <div className="container mx-auto flex items-center gap-40">
-          <div className="bg-white px-6 py-3 rounded-full flex items-center gap-10 w-fit cursor-pointer">
-            <div className="flex items-center gap-3">
-              <HamburgerSvg />
-              <p className="text-sm font-semibold">All Categories</p>
-            </div>
-            <div>
-              <DownArrowSvg dark={true} />
+          <div className="relative">
+            <button
+              onMouseEnter={() => setShowCategory(!showCategory)}
+              onMouseLeave={() => setShowCategory(!showCategory)}
+              className={` bg-white px-6 py-3 rounded-full flex items-center gap-16 w-fit cursor-pointer`}
+            >
+              <div className="flex items-center gap-3">
+                <HamburgerSvg />
+                <p className="text-sm font-semibold">All Categories</p>
+              </div>
+              <div>
+                <DownArrowSvg dark={true} />
+              </div>
+            </button>
+            <div
+              className={`bg-white p-6 rounded-3xl absolute w-full shadow-lg transition-all duration-500
+                ${
+                  showCategory
+                    ? 'z-10 translate-y-3 opacity-100'
+                    : '-z-10 translate-y-6 opacity-0'
+                }
+                `}
+            >
+              <ul
+                onMouseEnter={() => setShowCategory(!showCategory)}
+                onMouseLeave={() => setShowCategory(!showCategory)}
+                className="space-y-4"
+              >
+                {categories?.map((category, index) => (
+                  <li key={category?.title} className="cursor-pointer w-full">
+                    <Link
+                      className="block hover:font-bold transition-all duration-300"
+                      to={category?.path}
+                    >
+                      {category?.title}
+                      {index < categories.length - 1 && (
+                        <div className="h-[0.5px] w-full bg-[#E5E5E5] mt-3" />
+                      )}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
           <ul className="flex items-center gap-8">
