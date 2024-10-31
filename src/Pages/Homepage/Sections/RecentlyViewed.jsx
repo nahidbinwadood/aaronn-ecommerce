5a245c992e68f91a85216f15e3314ede3f1ed334
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-10-31 12:50:48 +0600

    updated slider

--- src/Pages/Homepage/Sections/RecentlyViewed.jsx
+++ src/Pages/Homepage/Sections/RecentlyViewed.jsx
@@ -1,9 +1,6 @@
-import React, { useState } from 'react';
-// Import Swiper React components
+import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import required modules
 import { Pagination, Navigation } from 'swiper/modules';
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import ItemCard from '@/Components/Cards/ItemCard';
@@ -11,177 +8,79 @@ import {
   LeftArrowNavSvg,
   RightArrowNavSvg,
 } from '@/Components/Svg Container/SvgContainer';
-
 const allProducts = [
   {
-    image: 'https://i.imgur.com/ia0QnBP.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.33,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/pdJByqZW/product-1.jpg',
+    title: 'Xiaomi Mijia Vacuum Cleaner',
+    review: 4,
+    price: 163.07,
+    previousPrice: 247.92,
   },
   {
-    image: 'https://i.imgur.com/SQQlarb.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/3RrFGmSg/product-2.jpg',
+    title: 'Samsung PowerBot Vacuum',
+    review: 3,
+    price: 240.23,
+    previousPrice: 338.37,
   },
   {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/Jr86mBq7/product-3.jpg',
+    title: 'Dyson V11 Vacuum Cleaner',
+    review: 3,
+    price: 165.46,
+    previousPrice: 224.45,
   },
   {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/zfrjJ9T2/product-4.jpg',
+    title: 'Eufy RoboVac 11S',
+    review: 1,
+    price: 343.03,
+    previousPrice: 400.19,
   },
   {
-    image: 'https://i.imgur.com/ia0QnBP.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/9f1WmwD4/product-5.jpg',
+    title: 'iRobot Roomba 675',
+    review: 4,
+    price: 302.69,
+    previousPrice: 376.84,
   },
   {
-    image: 'https://i.imgur.com/SQQlarb.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/xTbTvd47/product-6.jpg',
+    title: 'Shark Navigator Lift-Away',
+    review: 1,
+    price: 207.62,
+    previousPrice: 303.89,
   },
   {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
+    image: 'https://i.postimg.cc/g2Vz7TdH/product-7.jpg',
+    title: 'Bissell CleanView Vacuum',
     review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    price: 233.7,
+    previousPrice: 308.45,
   },
   {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/JzV7PLwh/product-8.jpg',
+    title: 'Hoover WindTunnel',
+    review: 1,
+    price: 334.78,
+    previousPrice: 389.11,
   },
   {
-    image: 'https://i.imgur.com/ia0QnBP.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/ZqJbzx5t/product-9.jpg',
+    title: 'LG CordZero Vacuum',
+    review: 2,
+    price: 311.16,
+    previousPrice: 367.6,
   },
   {
-    image: 'https://i.imgur.com/SQQlarb.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/ia0QnBP.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/SQQlarb.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/ia0QnBP.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/SQQlarb.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/ia0QnBP.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/SQQlarb.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
-  },
-  {
-    image: 'https://i.imgur.com/LRfGo6D.png',
-    title: 'Xiaomi Mijia vacuum Cleaner',
-    review: 5,
-    price: 196.34,
-    previousPrice: 330.69,
+    image: 'https://i.postimg.cc/L4W8gPNL/product-10.jpg',
+    title: 'Ecovacs Deebot N79S',
+    review: 1,
+    price: 244.7,
+    previousPrice: 316.18,
   },
 ];
+
 const RecentlyViewed = () => {
   const [swiperRef, setSwiperRef] = useState(null);
 
@@ -212,13 +111,14 @@ const RecentlyViewed = () => {
         />
 
         {/* sliders */}
-        <div className="container mx-auto py-10">
+        <div className="container mx-auto py-10 rounded-2xl">
           <Swiper
+            loop={true}
             onSwiper={setSwiperRef}
             slidesPerView={4}
             spaceBetween={30}
             modules={[Pagination, Navigation]}
-            className="mySwiper relative mb-16"
+            className="mySwiper relative mb-16 rounded-2xl"
           >
             {allProducts.map((item, idx) => (
               <SwiperSlide key={idx}>
