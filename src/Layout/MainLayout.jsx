de43c63aafca253c463913b107040265f5d9a965
Akm Nahid Bin Wadood <nahidbinwadood@gmail.com>
2024-11-02 09:23:08 +0600

    added message link on homepage

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,11 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { Link, Outlet, ScrollRestoration } from 'react-router-dom';
 import Navbar from './../Shared/Navbar';
 import Footer from '../Shared/Footer';
+import { FaRegMessage } from 'react-icons/fa6';
 
 const MainLayout = () => {
   return (
-    <>
+    <div className="relative">
       <ScrollRestoration />
       <nav className="font-poppins">
         <Navbar />
@@ -15,7 +16,12 @@ const MainLayout = () => {
       <footer className="font-poppins">
         <Footer />
       </footer>
-    </>
+
+      {/* message icon */}
+      <Link to="/dashboard/message-center" className="fixed right-10 bottom-10 bg-[#FF6F23] p-5 rounded-full z-50">
+        <FaRegMessage className='text-white size-5' />
+      </Link>
+    </div>
   );
 };
 
